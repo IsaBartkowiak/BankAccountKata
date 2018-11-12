ef81fa8ebf7f45164b8a375b78c502e5f2ad3224
Isa Bartkowiak <bartkowiak.isadora@gmail.com>
2018-11-12 02:18:23 +0100

    Statement should be correct - test OK

--- src/main/java/com/bankaccount/kata/model/Statement.java
+++ src/main/java/com/bankaccount/kata/model/Statement.java
@@ -1,4 +1,27 @@
 package com.bankaccount.kata.model;
+import java.time.LocalDateTime;
 
 public class Statement {
+
+    private LocalDateTime localDate;
+    private double amount;
+    private double balance;
+    private String operationType;
+
+    Statement(LocalDateTime date, double amount, double balance){
+        this.localDate = date;
+        this.amount = amount;
+        this.balance = balance;
+        this.operationType = amount > 0 ? "DEPOSIT" : "WITHDRAWAL";
+    }
+
+    @Override
+    public String toString() {
+        return this.operationType + " | " + this.localDate + " | " + this.amount + " | " + this.balance;
+    }
+
+    //for test
+    public String getOperationType() {
+        return this.operationType;
+    }
 }
