088912c1289302fae1fc9b74c3b8124c594a548b
Isa Bartkowiak <bartkowiak.isadora@gmail.com>
2018-11-12 09:20:59 +0100

    Add getter for date, amount, balance fields

--- src/main/java/com/bankaccount/kata/model/Statement.java
+++ src/main/java/com/bankaccount/kata/model/Statement.java
@@ -23,8 +23,20 @@ public class Statement {
         return this.operationType + " | " + this.localDate + " | " + this.amount + " | " + this.balance;
     }
 
-    //for test
+    public LocalDateTime getLocalDate() {
+        return this.localDate;
+    }
+
+    public double getAmount() {
+        return this.amount;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
     public String getOperationType() {
         return this.operationType;
     }
+
 }
