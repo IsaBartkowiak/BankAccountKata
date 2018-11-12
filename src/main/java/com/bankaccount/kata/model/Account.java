4b828f295eda2c539e0fed6ab629619667a4ad84
Isa Bartkowiak <bartkowiak.isadora@gmail.com>
2018-11-12 03:02:07 +0100

    Account model should be correct - Refactor test OK

--- src/main/java/com/bankaccount/kata/model/Account.java
+++ src/main/java/com/bankaccount/kata/model/Account.java
@@ -1,4 +1,38 @@
 package com.bankaccount.kata.model;
 
+import java.util.List;
+
 public class Account {
+    private final long id;
+    private final String name;
+    public double balance;
+    public final History history;
+
+    public Account(long id, String name, double balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+        this.history = new History();
+        this.history.addLine(0, balance);
+    }
+
+    public List<Statement> getHistory() {
+        return this.history.getStatements();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
 }
